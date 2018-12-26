9515479c15943d1029529bb123420c77182ba690
Jacob Burenstam <burenstam@gmail.com>
2018-12-26 23:15:42 +0100

    Cleaner data structure

--- src/debug-info.js
+++ src/debug-info.js
@@ -1,32 +1,86 @@
-const bowser = require('bowser');
-const browserData = bowser.getParser(window.navigator.userAgent).parsedResult;
+const Bowser = require('bowser');
+const BROWSER = Bowser.getParser(window.navigator.userAgent).parsedResult;
 
-export function browserInfo() {
-  const navigatorData = {
-    browser: navigator.appName,
-    codename: navigator.appCodeName,
-    version: navigator.appVersion,
-    online: navigator.onLine,
-    language: navigator.language,
-    platform: navigator.platform,
-    product: navigator.product,
-    userAgent: navigator.userAgent
-  };
+const browser = () => new Promise(resolve => resolve({
+  browser: {
+    ...BROWSER.browser,
+    shortVersion: BROWSER.browser.version.split('.')[0],
+    webdriver: navigator.webdriver,
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+}));
 
-  const pluginsData = {};
-  for (var key in navigator.plugins) {
+const device = () => new Promise(resolve => resolve({
+  device: {
+    hardwareConcurrency: navigator.hardwareConcurrency,
+    memoryGB: navigator.deviceMemory,
+    resolution: `${screen.availWidth}x${screen.availHeight}`,
+    width: screen.availWidth,
+    height: screen.availHeight,
+    colorDepth: screen.colorDepth,
+    isRetina: (window.devicePixelRatio || 0) > 1 || false,
+  }
+}))
+
+const languages = () => new Promise(resolve => resolve({
+  language: navigator.language,
+  languages: navigator.languages,
+}));
+
+const features = () => new Promise(resolve => resolve({
+  features: {
+    cookieEnabled: navigator.cookieEnabled,
+    javaEnabled: navigator.javaEnabled(),
+    websocketsEnabled: ('WebSocket' in window || 'MozWebSocket' in window),
+  }
+}));
+
+const plugins = () => new Promise(resolve => {
+  const data = {};
+  for (let key in navigator.plugins) {
     if (!navigator.plugins.hasOwnProperty(key)) continue;
 
-    pluginsData[navigator.plugins[key].name] = navigator.plugins[key].description;
+    data[navigator.plugins[key].name] = navigator.plugins[key].description;
   }
 
-  return {
-    ...browserData,
-    browser: {
-      shortVersion: browserData.browser.version.split('.')[0],
-      ...browserData.browser
-    },
-    navigator: navigatorData,
-    plugins: pluginsData
-  };
-}
+  resolve({ plugins: data });
+});
+
+const rootData = () => new Promise(resolve => resolve({
+  ...BROWSER,
+  userAgent: navigator.userAgent,
+  vendor: navigator.vendor,
+  online: navigator.onLine,
+  doNotTrack: navigator.doNotTrack,
+  userAgent: navigator.userAgent
+}));
+
+const battery = () => new Promise(resolve => {
+  if (!navigator.getBattery) resolve({ battery: {} });
+
+  navigator.getBattery()
+    .then(battery => resolve({
+      battery: {
+        charging: battery.charging,
+        level: battery.level,
+      }
+    }));
+});
+
+export const browserInfo = () => new Promise(resolve => {
+  const dataParts = [
+    rootData(),
+    browser(),
+    languages(),
+    device(),
+    battery(),
+    plugins(),
+    features()
+  ];
+
+  Promise.all(dataParts).then(values => {
+    const data = values.reduce((acc, value) => Object.assign(acc, value), {});
+    resolve({ ...data })
+  });
+});
--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ import copyToClipboard from './copy-to-clipboard';
 // Set page favicon
 setFavicon(favicon);
 
-const info = browserInfo();
 onReady(() => {
-  const logoHref = browserLogos[info.browser.name];
-  if (logoHref) {
-    appendTo('#browser-logo', createImage(logoHref))
-  }
-  setText('#browser-name', info.browser.name);
-  setText('#browser-version', info.browser.shortVersion);
-  setText('#debug-info-json', JSON.stringify(info, null, 2));
+  browserInfo().then(info => {
+    const logoHref = browserLogos[info.browser.name];
+    if (logoHref) {
+      appendTo('#browser-logo', createImage(logoHref))
+    }
+    setText('#browser-name', info.browser.name);
+    setText('#browser-version', info.browser.shortVersion);
+    setText('#debug-info-json', JSON.stringify(info, null, 2));
+  })
 });
 
 onReady(() => {
